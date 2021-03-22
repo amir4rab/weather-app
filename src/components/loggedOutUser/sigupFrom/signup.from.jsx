3ffd28650770bc86cb5f85c5e7982efebceb8149
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-03-22 15:42:47 +0430

    signupform checks compeleted

--- src/components/loggedOutUser/sigupFrom/signup.from.jsx
+++ src/components/loggedOutUser/sigupFrom/signup.from.jsx
@@ -5,23 +5,92 @@ import passwordValidator  from 'password-validator';
 
 import '../component.styles.scss';
 
+//**  password validator settings  **//
+
+const passwordSchema = new passwordValidator();
+passwordSchema
+    .is().min(8)
+    .is().max(24)
+    .has().uppercase()
+    .has().lowercase()
+    .has().digits()
+    .has().not().spaces();
+
+//**  password validator settings  **//
+
 const SignupFrom = ({ initSignup }) => {
+
+    //**   Email word sate and checks  **//
+
     const [ emailState, setEmailState ] = useState({
         isTrue: false,
         touched: false,
         err: null
     });
     const emailInput = createRef();
-    
+    const checkEmail = () => {
+        if ( validator.validate(emailInput.current.value) ){
+            setEmailState(currState => ({
+                ...currState,
+                touched: true,
+                isTrue: true
+            }));
+        } else {
+            setEmailState(currState => ({
+                ...currState,
+                touched: true,
+                isTrue: false
+            }));
+        }
+    }
+
+    //**   Email word sate and checks  **//
+
+
+    //**   pass word sate and checks  **//
+
     const [ password, setPassword ] = useState({
         isTrue: false,
         repeateIsFalse: false,
+        repeateIstouched: false,
         touched: false,
         err: null
     });
     const passwordInput = createRef();
-    
     const passwordRepeatInput = createRef();
+    const checkPassword = () => {
+        if( passwordSchema.validate(passwordInput.current.value) ){
+            setPassword(currState => ({
+                ...currState,
+                touched: true,
+                isTrue: true
+            }));
+        } else {
+            setPassword(currState => ({
+                ...currState,
+                touched: true,
+                isTrue: false
+            }));
+        }
+    }
+    const checkPasswordRepeat = () => {
+        if( passwordInput.current.value === passwordRepeatInput.current.value ){
+            setPassword(currState => ({
+                ...currState,
+                repeateIstouched: true,
+                repeateIsFalse: false
+            }));
+        } else {
+            setPassword(currState => ({
+                ...currState,
+                repeateIstouched: true,
+                repeateIsFalse: true
+            }));
+        }
+    }
+    
+    //**   pass word sate and checks  **//
+    
 
     const initSignupFn = () => {
         initSignup(
@@ -30,17 +99,6 @@ const SignupFrom = ({ initSignup }) => {
         );
     }
 
-    const checkPasswordRepeat = (e) => {
-        // console.log( e )
-        if ( e.target.value !== passwordInput.current.value ) {
-            console.log('dont match')
-        } else {
-            console.log('math')
-        }
-    }
-
-    console.log(validator.validate('@test.com'));
-
     return (
         <form className={ `main` } onSubmit={ e => e.preventDefault() }>
             <h4 className={ `title` }>
@@ -48,12 +106,26 @@ const SignupFrom = ({ initSignup }) => {
             </h4>
             <div className={ `inputGroup` }>
                 <label className={ `inputGroup_label` }>Email</label>
-                <input className={ `inputGroup_input ${ !emailState.isTrue && emailState.touched ? `false` : '' } ` } ref={ emailInput } type="email"/>
+                <input 
+                    className={ 
+                        `inputGroup_input 
+                        ${ !emailState.isTrue && emailState.touched ? `false` : '' }
+                        ${ emailState.isTrue ? `true` : '' }`
+                    } 
+                    ref={ emailInput } 
+                    type="email"
+                    onKeyUp={checkEmail}
+                    />
             </div>
             <div className={ `inputGroup` }>
                 <label className={ `inputGroup_label` }>Password</label>
                 <input 
-                    className={ `inputGroup_input ${ ( !password.isTrue && password.touched ) || password.repeateIsFalse ? `false` : '' } ` } 
+                    className={[ 
+                        `inputGroup_input`,
+                        `${ !password.isTrue && password.touched  ? `false` : null }`,
+                        `${ password.isTrue ? `true` : null }`, 
+                    ].join(' ')}
+                    onKeyUp={ checkPassword }
                     ref={ passwordInput } 
                     type="password"
                 />
@@ -61,13 +133,21 @@ const SignupFrom = ({ initSignup }) => {
             <div className={ `inputGroup` }>
                 <label className={ `inputGroup_label` }>repeat your password</label>
                 <input 
-                    className={ `inputGroup_input` } 
+                    className={[
+                        `inputGroup_input`,
+                        `${ password.repeateIsFalse && password.repeateIstouched ? `false` : null }`,
+                        `${ !password.repeateIsFalse && password.repeateIstouched ? `true` : null }`,
+                    ].join(' ')} 
                     ref={ passwordRepeatInput } 
                     onKeyUp={ checkPasswordRepeat } 
                     type="password" show
                 />
             </div>
-            <button className={`submitBtn`} onClick={initSignupFn}>
+            <button 
+                className={ `submitBtn` } 
+                onClick={ initSignupFn }
+                disabled={ !((!password.repeateIsFalse && password.repeateIstouched) && password.isTrue && emailState.isTrue) }
+            >
                 Submit
             </button>
         </form>
