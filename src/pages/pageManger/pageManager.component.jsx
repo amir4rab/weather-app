35f421a89bd315f3501538190c5c33398b2f167b
amir4rab <60965542+amir4rab@users.noreply.github.com>
2021-04-07 10:27:45 +0430

    bug fixes

--- src/pages/pageManger/pageManager.component.jsx
+++ src/pages/pageManger/pageManager.component.jsx
@@ -1,6 +1,8 @@
 import { lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
+import classes from './pageManager.module.scss';
+
 import NavbarDesktop from '../../components/navbarDesktop/navbarDesktop.component';
 import NavbarMobile from '../../components/navbarMobile/navbarMobile.component';
 import LoadingAnimation from './loadingAnimation/loadingAnimation.component';
@@ -11,28 +13,32 @@ const SettingsPage = lazy(() => import('../settingsPage/settings.page'));
 
 const PageManager = props => {
     return (
-        <div>
-            <NavbarDesktop />
-            <Switch>
-                <Route path='/' exact>
-                    <Suspense fallback={<LoadingAnimation />}>
-                        <HomePage />
-                    </Suspense>
-                </Route>
-                <Route path='/account' exact>
-                    <Suspense fallback={<LoadingAnimation />}>
-                        <AccountPage />
-                    </Suspense>
-                </Route>
-                <Route path='/settings' exact>
-                    <Suspense fallback={<LoadingAnimation />}>
-                        <SettingsPage />
-                    </Suspense>
-                </Route>
-                <Route path='**'>
-                    <Redirect to='/' />
-                </Route>
-            </Switch>
+        <div className={ classes.main }>
+            <div className={ classes.navbarTop }>
+                <NavbarDesktop />
+            </div>
+            <div className={ classes.inner }>
+                <Switch>
+                    <Route path='/' exact>
+                        <Suspense fallback={<LoadingAnimation />}>
+                            <HomePage />
+                        </Suspense>
+                    </Route>
+                    <Route path='/account' exact>
+                        <Suspense fallback={<LoadingAnimation />}>
+                            <AccountPage />
+                        </Suspense>
+                    </Route>
+                    <Route path='/settings' exact>
+                        <Suspense fallback={<LoadingAnimation />}>
+                            <SettingsPage />
+                        </Suspense>
+                    </Route>
+                    <Route path='**'>
+                        <Redirect to='/' />
+                    </Route>
+                </Switch>
+            </div>
             <NavbarMobile />
         </div>
     );
